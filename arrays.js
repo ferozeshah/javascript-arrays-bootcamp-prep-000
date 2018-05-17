fadfc8aa2742a3025f588c09def360d2a98c5788
Feroze Shah <fshah@mit.edu>
2018-05-17 08:01:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -45,6 +45,7 @@ function destructivelyRemoveElementFromEndOfArray(arr){
 
 function removeElementFromEndOfArray(arr){
   var r = arr.slice(0,arr.length-1)
+  return r
 }
 
 cars = ["Honda","Toyota"]
